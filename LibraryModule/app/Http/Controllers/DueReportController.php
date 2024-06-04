f1f67e452b407e1eea01ba074ae93a331ec0c880
Vergil <90892527+hachikyuji@users.noreply.github.com>
2024-06-04 20:17:32 +0800

    Update DueReportController.php

--- LibraryModule/app/Http/Controllers/DueReportController.php
+++ LibraryModule/app/Http/Controllers/DueReportController.php
@@ -15,8 +15,8 @@ class DueReportController extends Controller
 {
     public function overdueBooks(Request $request)
     {
-        $overdueHistoriesQuery = AccountHistory::select('account_history.*', 'users.name as user_name')
-            ->join('users', 'users.email', '=', 'account_history.email');
+        $overdueHistoriesQuery = AccountHistory::select('library_account_history.*', 'users.name as user_name')
+            ->join('users', 'users.email', '=', 'library_account_history.email');
     
         $searchParam = $request->query('q');
     
@@ -24,12 +24,12 @@ class DueReportController extends Controller
             $overdueHistoriesQuery->where(function ($query) use ($searchParam) {
                 $query
                     ->orWhere('users.name', 'like', "%$searchParam%")
-                    ->orWhere('account_history.email', 'like', "%$searchParam%")
+                    ->orWhere('library_account_history.email', 'like', "%$searchParam%")
                     ->orWhere('books_borrowed', 'like', "%$searchParam%")
                     ->orWhere('borrowed_date', 'like', "%$searchParam%")
                     ->orWhere('book_deadline', 'like', "%$searchParam%")
-                    ->orWhere('account_history.college', 'like', "%$searchParam%") 
-                    ->orWhere('account_history.course', 'like', "%$searchParam%");
+                    ->orWhere('library_account_history.college', 'like', "%$searchParam%") 
+                    ->orWhere('library_account_history.course', 'like', "%$searchParam%");
             });
         }
     
@@ -48,19 +48,19 @@ class DueReportController extends Controller
     
         Log::info("Search Param: $filter");
     
-        $overdueHistoriesQuery = AccountHistory::select('account_history.*', 'users.name as user_name')
-            ->join('users', 'users.email', '=', 'account_history.email');
+        $overdueHistoriesQuery = AccountHistory::select('library_account_history.*', 'users.name as user_name')
+            ->join('users', 'users.email', '=', 'library_account_history.email');
     
         if ($filter) {
             $overdueHistoriesQuery->where(function ($query) use ($filter) {
                 $query
                     ->orWhere('users.name', 'like', "%$filter%")
-                    ->orWhere('account_history.email', 'like', "%$filter%")
+                    ->orWhere('library_account_history.email', 'like', "%$filter%")
                     ->orWhere('books_borrowed', 'like', "%$filter%")
                     ->orWhere('borrowed_date', 'like', "%$filter%")
                     ->orWhere('book_deadline', 'like', "%$filter%")
-                    ->orWhere('account_history.college', 'like', "%$filter%") 
-                    ->orWhere('account_history.course', 'like', "%$filter%");
+                    ->orWhere('library_account_history.college', 'like', "%$filter%") 
+                    ->orWhere('library_account_history.course', 'like', "%$filter%");
             });
         }
     
